d8961ca23ca7ec75a45fe40346e8b5e7ce3c1d70
scherbak-electronics <scherbak.electronics@gmail.com>
2023-06-13 14:40:20 +0300

    first commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Welcome to Trading Control!</h1>
+        <p>This is the landing page of the Trading Control app.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Trading Control</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+<div id="app"></div>
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
